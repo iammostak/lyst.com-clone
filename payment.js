16b8c270952bb03c6bfa3df93fd236f1086aa189
iammostak <mdmostafijur85@gmail.com>
2022-05-09 20:18:03 +0530

    index page linked

--- payment.js
+++ payment.js
@@ -4,5 +4,8 @@ var pButton = document.querySelector('#pB');
 pButton.addEventListener('click', function() {
    if(pForm.cardNum.value.length != 16) alert('Please enter a valid card number');
    else if(pForm.cvv.value.length != 3) alert('Wrong CVV');
-   else alert('Payment Successful, Order Placed');
+   else {
+      alert('Payment Successful, Order Placed');
+      window.location.href = 'index.html';
+   }
 })
